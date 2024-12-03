2b1e3f89e0e0c5de37728b1d63ed423d33d3114c
AHMAD RIAN <alriansr@gmail.com>
2024-12-03 19:36:29 +0700

    push

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:katalis/views/home_screen.dart';
 import 'package:katalis/views/login/login_screen.dart';
 import 'package:katalis/views/welcome_screen.dart'; // Import SplashScreen
 import 'package:katalis/views/onboarding_screen.dart'; // Import OnboardingScreen
-import 'package:katalis/views/home _screen.dart'; // Jika Anda sudah membuat HomeScreen terpisah
 
 void main() {
   runApp(MyApp());
@@ -15,14 +15,12 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Katalis App',
       theme: ThemeData(primarySwatch: Colors.blue),
-      initialRoute:
-          '/welcome', // Ganti dengan '/onboarding' jika onboarding ingin ditampilkan dulu
+      initialRoute: '/welcome',
       routes: {
-        '/home': (context) => HomeScreen(), // Beranda setelah login
+        '/home': (context) => HomeScreen(),
         '/login': (context) => LoginScreen(),
-        '/onboarding': (context) => OnboardingScreen(), // Halaman onboarding
-        '/welcome': (context) =>
-            WelcomeScreen(), // Halaman splash screen atau welcome screen
+        '/onboarding': (context) => OnboardingScreen(),
+        '/welcome': (context) => WelcomeScreen(),
       },
     );
   }
